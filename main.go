c1f395cbfbc2f7e99ba476093ad7f826cecf7e96
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2023-03-24 06:37:56 -0700

    must create table before you can check them

--- main.go
+++ main.go
@@ -13,6 +13,8 @@ func main() {
 	Create_Reviews_Tables()
 	Insert_Initial_Comments()
 	Create_Estimate_Tables()
+	Create_Photos_Tables()
+	Create_Videos_Tables()
 
 	e := echo.New()
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
